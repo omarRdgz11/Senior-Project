2f60db3efc4dba772e6596539a2274fda76b8913
Omar <omar.rdgz04@gmail.com>
2025-11-25 13:09:44 -0700

    Dashboard Implementation (no backend yet)

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -8,8 +8,9 @@ import Omar from "../pages/About/People/Omar";
 import Linh from "../pages/About/People/Linh";
 import Analee from "../pages/About/People/Analee"
 import WildfireMapPage from "../pages/WildfireMap/WildfireMapPage";
-import TestRawDetections from "../pages/TestRawDetections";
-import ModelTest from "../pages/ModelTest";
+import TestRawDetections from "../pages/TestPages/TestRawDetections";
+import ModelTest from "../pages/TestPages/ModelTest";
+import DashboardTest from "../pages/TestPages/DashboardTest";
 // Add more imports as new teammates create files
 
 const NotFound = () => (
@@ -34,6 +35,7 @@ export const router = createBrowserRouter([
       { path: "about/people/linh", element: <Linh /> },
       { path: "about/people/Analee", element: <Analee />},
       { path: "ModelTest", element: <ModelTest />},
+      { path: "dashboardtest", element: <DashboardTest /> },
 
       { path: "*", element: <NotFound /> },
     ],
