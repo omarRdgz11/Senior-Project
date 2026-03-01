6bcf0c3740bb1d8058d1180a6d6c2faec8fa5266
Linh Huynh <133999148+asterphine@users.noreply.github.com>
2026-03-01 13:44:49 -0600

    change prob and thresh back

--- frontend/src/pages/WildfireMap/WildfireMapPage.tsx
+++ frontend/src/pages/WildfireMap/WildfireMapPage.tsx
@@ -573,12 +573,12 @@ export default function WildfireMapPage() {
                 <br />
                 <strong>Date:</strong> {predictionMarker.date}
                 <br />
-                {/* <strong>Probability:</strong>{" "}
+                <strong>Probability:</strong>{" "}
                 {(predictionMarker.probability * 100).toFixed(1)}%
                 <br />
                 <strong>Threshold:</strong>{" "}
-                {predictionMarker.threshold.toFixed(2)} */}
-                const prob =
+                {predictionMarker.threshold.toFixed(2)}
+                {/* const prob =
                   typeof predictionMarker?.probability === "number"
                     ? (predictionMarker.probability * 100).toFixed(1)
                     : "—";
@@ -586,7 +586,7 @@ export default function WildfireMapPage() {
                 const thresh =
                   typeof predictionMarker?.threshold === "number"
                     ? predictionMarker.threshold.toFixed(2)
-                    : "—";
+                    : "—"; */}
                 <br />
                 <strong>Risk:</strong>{" "}
                 {predictionMarker.label === 1 ? "⚠️ High" : "✓ Low"}
